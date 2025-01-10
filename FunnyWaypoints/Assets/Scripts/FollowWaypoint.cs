e7f4fe4815ee8dd9984615461a2007c07ef73c03
awsuhime <awsuhime@gmail.com>
2025-01-10 01:57:03 -0800

    NewStuff

--- FunnyWaypoints/Assets/Scripts/FollowWaypoint.cs
+++ FunnyWaypoints/Assets/Scripts/FollowWaypoint.cs
@@ -8,25 +8,45 @@ public class FollowWaypoint : MonoBehaviour
     public int currentWayPoint = 0;
     public float speed = 10f;
     public float rotSpeed = 10f;
+    public float lookAhead = 10.0f;
+    GameObject tracker;
 
     void Start()
     {
+        tracker = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        DestroyImmediate(tracker.GetComponent<Collider>());
+        tracker.GetComponent<MeshRenderer>().enabled = false;
+        tracker.transform.position = transform.position;
+        tracker.transform.rotation = transform.rotation;
+    }
+
+    void ProgressTracker()
+    {
+        if (Vector3.Distance(transform.position, tracker.transform.position) < lookAhead)
+        {
+            if (Vector3.Distance(tracker.transform.position, transform.position) < 5f)
+            {
+                currentWayPoint++;
+                if (currentWayPoint == waypoints.Length)
+                {
+                    currentWayPoint = 0;
+                }
+            }
+
+            tracker.transform.LookAt(waypoints[currentWayPoint].transform);
+            tracker.transform.Translate(0, 0, speed * 2 * Time.deltaTime);
+        }
         
     }
+
+ 
 
     void Update()
     {
 
-       
-        if (Vector3.Distance(waypoints[currentWayPoint].transform.position, transform.position) < 5f)
-        {
-            currentWayPoint++;
-            if (currentWayPoint == waypoints.Length)
-            {
-                currentWayPoint = 0;
-            }
-        }
-        Quaternion LookAtWP = Quaternion.LookRotation(waypoints[currentWayPoint].transform.position - transform.position);
+        ProgressTracker();
+        
+        Quaternion LookAtWP = Quaternion.LookRotation(tracker.transform.position - transform.position);
         transform.rotation = Quaternion.Slerp(transform.rotation, LookAtWP, Time.deltaTime * rotSpeed);
         transform.Translate(0,0, speed * Time.deltaTime);
 
---
+++ FunnyWaypoints/Assets/Scripts/WaypointManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public struct Link
+{
+    public enum direction { UNI, BI }
+    public GameObject node1;
+    public GameObject node2;
+    public direction dir;
+}
+public class WaypointManager : MonoBehaviour
+{
+    public GameObject[] waypoints;
+    public Link[] links;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
