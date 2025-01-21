8ed1838755f62d7407d25ce3174d392b08b7489c
SmallBowl <awsuhime@gmail.com>
2025-01-21 15:19:46 -0800

    Imported Assets

--- FunnyWaypoints/Assets/Scripts/FollowWaypoints.cs
+++ FunnyWaypoints/Assets/Scripts/FollowWaypoints.cs
@@ -1,70 +1,95 @@
-using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class FollowWaypoints : MonoBehaviour
-{
-    Transform goal;
-    public float speed = 5.0f;
-    float accuracy = 1.0f;
-    public float rotSpeed = 2.0f;
+public class FollowWaypoints : MonoBehaviour {
 
-    public GameObject wpManager;
+    // Transform goal;
+    // float speed = 5.0f;
+    // float accuracy = 5.0f;
+    // float rotSpeed = 2.0f;
+
     GameObject[] wps;
     GameObject currentNode;
-    int currentWP = 0;
-    Graph g;
-    void Start()
-    {
+    NavMeshAgent agent;
+
+    // int currentWP = 0;
+    // Graph g;
+
+    public GameObject wpManager;
+
+    void Start() {
+        Time.timeScale = 5.0f;
         wps = wpManager.GetComponent<WaypointManager>().waypoints;
-        g = wpManager.GetComponent<WaypointManager>().graph;
-        currentNode = wps[3];
+        // g = wpManager.GetComponent<WPManager>().graph;
+        currentNode = wps[0];
+
+        agent = GetComponent<NavMeshAgent>();
+
+        // Invoke("GotoRuin", 2.0f);
     }
 
-    public void GoToHeli()
-    {
-        g.AStar(currentNode, wps[3]);
-        currentWP = 0;
+    public void GotoHeli() {
 
-        
+        //g.AStar(currentNode, wps[0]);
+        //currentWP = 0;
+        GotoHere(0);
     }
 
-    public void GoToSpire()
-    {
-        g.AStar(currentNode, wps[2]);
-        currentWP = 0;
+    public void GotoRuin() {
+
+        //g.AStar(currentNode, wps[7]);
+        //currentWP = 0;
+        GotoHere(7);
     }
 
-    public void GoToNowhere()
-    {
-        g.AStar(currentNode, wps[5]);
-        currentWP = 0;
+    public void GotoRock() {
+
+        //g.AStar(currentNode, wps[1]);
+        //currentWP = 0;
+        GotoHere(1);
     }
 
-    public void GoToFactory()
-    {
-        g.AStar(currentNode, wps[4]);
-        currentWP = 0;
+    public void GotoFactory() {
+
+        //g.AStar(currentNode, wps[4]);
+        //currentWP = 0;
+        GotoHere(4);
     }
 
-    void LateUpdate()
-    {
-        if(g.pathList.Count == 0 || currentWP == g.pathList.Count){
-            return;
-        }
-        if (Vector3.Distance(g.pathList[currentWP].getId().transform.position, transform.position) < accuracy)
-        {
-            currentNode = g.pathList[currentWP].getId();
-            currentWP++;
-        }
-        if (currentWP < g.pathList.Count)
-        {
-            goal = g.pathList[currentWP].getId().transform;
-            Vector3 lookAtGoal = new Vector3(goal.position.x, transform.position.y, goal.position.z);
-            Vector3 direction = lookAtGoal - transform.position;
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * rotSpeed);
-            transform.Translate(0, 0, speed * Time.deltaTime);
-        }
+    void GotoHere(int value) {
+
+        agent.SetDestination(wps[value].transform.position);
+    }
+
+    void LateUpdate() {
+
+        //if (g.pathList.Count == 0 || currentWP == g.pathList.Count) return;
+
+        //currentNode = g.getPathPoint(currentWP);
+
+        //if (Vector3.Distance(g.pathList[currentWP].getID().transform.position, transform.position) < accuracy) {
+
+        //    currentWP++;
+        //}
+
+        //if (currentWP < g.pathList.Count) {
+
+        //    goal = g.pathList[currentWP].getID().transform;
+        //    Vector3 lookAtGoal = new Vector3(
+        //        goal.position.x,
+        //        transform.position.y,
+        //        goal.position.z);
+
+        //    Vector3 direction = lookAtGoal - this.transform.position;
+
+        //    transform.rotation = Quaternion.Slerp(
+        //        this.transform.rotation,
+        //        Quaternion.LookRotation(direction),
+        //        Time.deltaTime * rotSpeed);
+
+        //    transform.Translate(0.0f, 0.0f, speed * Time.deltaTime);
+        //}
     }
 }
